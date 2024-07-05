c5e12e3bc42fa0cf3d3a4bcb92bac83f505616dc
eladgl <59554824+eladgl@users.noreply.github.com>
2024-07-05 12:24:12 +0300

    add pages

--- Tourism-Guide-UI/src/App.jsx
+++ Tourism-Guide-UI/src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-const App = () => {
-  return <div>Hello</div>;
-};
+import { HomePage } from "./pages/homePage";
+
+import NavBar from "./components/navBar";
+import TopNavBar from "./components/topNavBar";
+import Footer from "./components/footer";
+
+const App = () => (
+  <>
+    <div class="flex flex-col min-h-screen bg-gradient-to-br from-blue-800 to-blue-900">
+      <Router>
+        <TopNavBar />
+        <div class="flex flex-row flex-1 w-full bg-gray-100">
+          <div>
+            <NavBar className="hidden md:block" />
+          </div>
+          <div className="w-full h-full ">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </div>
+    <Footer />
+  </>
+);
 
 export default App;
---
+++ Tourism-Guide-UI/src/components/topNavBar.jsx
@@ -0,0 +1,5 @@
+const TopNavBar = () => {
+  return "";
+};
+
+export default TopNavBar;
