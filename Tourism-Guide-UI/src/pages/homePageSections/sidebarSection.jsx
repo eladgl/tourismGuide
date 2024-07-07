1b99975c681f39f8d0d4902c004f6fb4840a173e
whisper120 <84220690+whisper120@users.noreply.github.com>
2024-07-07 21:32:36 +0300

    added sidebar in home page

---
+++ Tourism-Guide-UI/src/pages/homePageSections/sidebarSection.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Label from "../../components/label";
+
+const SidebarSection = () => {
+
+    const vacationTexts = [
+        {
+            text: "Explore tourist destinations in Banda Aceh",
+            url: "#"
+        },
+        {
+            text: "Cheap Padang specialty food restaurant",
+            url: "#"
+        },
+        {
+            text: "Mountain hiking in central Java",
+            url: "#"
+        }
+    ];
+
+    return (
+        <div className="SidebarSection">
+            <Label className="SidebarSection-label">Popular</Label>
+            <br />
+            <div style={{ maxWidth: '300px' }}>
+                {vacationTexts.map((item, index) => (
+                    <React.Fragment key={index}>
+                        <p style={{ fontWeight: 'bold', whiteSpace: 'normal', wordWrap: 'break-word' }}>
+                            <a href={item.url} rel="noopener noreferrer">{item.text}</a>
+                        </p>
+                        <p style={{ fontWeight: 'bold' }}>16 reviews</p>
+                        {index < vacationTexts.length - 1 && <hr style={{ width: '50%', margin: '8px 0' }} />}
+                    </React.Fragment>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default SidebarSection;
