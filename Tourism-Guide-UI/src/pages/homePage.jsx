5217475206028be4c07fbd6421d752b00125e9d4
whisper120 <84220690+whisper120@users.noreply.github.com>
2024-07-07 21:40:07 +0300

    added sidebar in home page (#8)
    * added sidebar in home page
    
    * removed the lines

--- Tourism-Guide-UI/src/pages/homePage.jsx
+++ Tourism-Guide-UI/src/pages/homePage.jsx
@@ -1,6 +1,20 @@
 import Search from "../components/search";
 import HeroSection from "./homePageSections/heroSection";
 import ReviewsSection from "./homePageSections/reviewsSection";
+import SidebarSection from "./homePageSections/sidebarSection";
+import styled from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between; /* Align items to opposite ends */
+  width: 100%;
+`;
+
+const SectionWrapper = styled.div`
+  flex: 1; /* Make sure each section takes up equal space */
+  padding: 10px; /* Optional: Add some padding for spacing */
+`;
 
 const HomePage = () => {
   return (
@@ -9,7 +23,14 @@ const HomePage = () => {
       <div>
         <Search />
       </div>
-      <ReviewsSection />
+      <Row>
+        <SectionWrapper>
+          <ReviewsSection />
+        </SectionWrapper>
+        <SectionWrapper>
+          <SidebarSection />
+        </SectionWrapper>
+      </Row>
     </>
   );
 };
