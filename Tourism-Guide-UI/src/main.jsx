bc04a6c3e782496ad5c6dc152389953228e17256
eladgl <59554824+eladgl@users.noreply.github.com>
2024-07-08 09:27:39 +0300

    add new run configuration

--- Tourism-Guide-UI/src/main.jsx
+++ Tourism-Guide-UI/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./tailwindStyles/index.css";
 import "virtual:svg-icons-register";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
