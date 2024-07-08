6ee832389c7f31a967fdc15311cd606bd63d5d0e
Dmitry Simon <47256059+PyExtr@users.noreply.github.com>
2024-07-08 17:00:15 +0300

    toggle changes (#17)

--- Tourism-Guide-UI/src/components/toggleButton.jsx
+++ Tourism-Guide-UI/src/components/toggleButton.jsx
@@ -1,40 +1,69 @@
 import styled from "styled-components";
-
 import SvgIcon from "./svgIcon";
 
 const ToggleWrapper = styled.div`
   position: relative;
-  display: inline-block;
-  width: 28px;
-  height: 16px;
+  display: inline-flex;
+  align-items: center;
+  width: 100px; /* Adjust as needed to fit your layout */
+  height: 40px; /* Adjust as needed to fit your layout */
+`;
 
-  .icon {
+const StyledLabel = styled.label`
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+`;
+
+const StyledInput = styled.input`
+  opacity: 0;
+  width: 0;
+  height: 0;
+`;
+
+const StyledSlider = styled.div`
+  position: relative;
+  width: 50px;
+  height: 25px;
+  background-color: ${props => (props.checked ? "#4A90E2" : "#ccc")};
+  border-radius: 25px;
+  transition: background-color 0.3s;
+  &::after {
+    content: "";
     position: absolute;
-    right: 0;
-    transform: scale(0.5);
-    margin-right: 10px;
+    width: 21px;
+    height: 21px;
+    background-color: #fff;
+    border-radius: 50%;
+    top: 2px;
+    left: ${props => (props.checked ? "27px" : "2px")};
+    transition: left 0.3s;
   }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-40%);
+  ${props => (props.checked ? "right: 28px;" : "left: 28px;")}
+  transition: ${props => (props.checked ? "right 0.3s" : "left 0.3s")};
 `;
 
 const ToggleButton = ({ icons, onChange, checked }) => {
   return (
-    <div className="toggleButton">
-      <label className="inline-flex items-center cursor-pointer">
-        <input
-          type="checkbox"
-          value=""
-          className="sr-only peer"
-          onChange={onChange}
-          checked={checked}
-        />
-        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-        <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
-        <SvgIcon
-          name={checked ? icons[0] : icons[1]}
-          className="w-[20px] h-[20px] -translate-x-[32px]"
-        />
-      </label>
-    </div>
+    <ToggleWrapper>
+      <StyledLabel>
+        <StyledInput type="checkbox" onChange={onChange} checked={checked} />
+        <StyledSlider checked={checked}></StyledSlider>
+        <IconWrapper checked={checked}>
+          <SvgIcon
+            name={checked ? icons[0] : icons[1]}
+            className="w-[20px] h-[20px]"
+          />
+        </IconWrapper>
+      </StyledLabel>
+    </ToggleWrapper>
   );
 };
 
