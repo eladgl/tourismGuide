a5c0efc5cd88dc81de325d7c62f91600f496073e
Elad Goldenberg <59554824+eladgl@users.noreply.github.com>
2024-07-26 13:15:58 +0300

    cors for vercel (#35)

--- Tourism-Guide-SERVER/src/middleware/middleware.js
+++ Tourism-Guide-SERVER/src/middleware/middleware.js
@@ -1,7 +1,31 @@
 import cors from "cors";
 import bodyParser from "body-parser";
 
+const allowCors = (fn) => async (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins, or specify allowed origins
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  // Handle pre-flight requests
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  // Call the next middleware or handler
+  return await fn(req, res);
+};
+
 export default function applyMiddleware(app) {
-  app.use(cors());
+  app.use(express.json());
+  app.use((req, res, next) => allowCors(next)(req, res));
   app.use(bodyParser.json());
 }
