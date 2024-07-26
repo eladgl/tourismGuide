9bbe31f0a4558da4d8c94e3126af703c786fb60d
eladgl <59554824+eladgl@users.noreply.github.com>
2024-07-26 17:36:24 +0300

    try

--- Tourism-Guide-UI/src/access/configs/config.jsx
+++ Tourism-Guide-UI/src/access/configs/config.jsx
@@ -1,4 +1,4 @@
-const PORT = import.meta.env.VITE_API_URL ? "" : ":3001";
+const PORT = import.meta.env.VITE_API_URL ? ":443" : ":3001";
 const apiUrl = `${import.meta.env.VITE_API_URL || "http://localhost"}${PORT}`;
 console.log("apiUrl: " + apiUrl);
 export default {
